b31ac968e3870ecc827d20cb95db50432bd599a7
Naveen Mamidi <n.mamidi.247@westcliff.edu>
2024-08-25 14:55:38 -0700

    Coded and excuted:

---
+++ sw.js
@@ -0,0 +1,27 @@
+const cacheName = 'pwa-demo-v1';
+const filesToCache = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/js/main.js',
+    '/images/icon-192x192.png',
+    '/images/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+                return cache.addAll(filesToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
